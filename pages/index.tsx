8d936a76a00dc4a4c6b289714ed9fe8d300cf974
Callum Morris <me@callum.io>
2021-02-15 20:51:40 +0000

    Intial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Valheim Tools</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Valheim Tools
+        </h1>
+
+        <p className={styles.description}>
+          Coming Soon
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://github.com/team-solar/valheim-tools"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Source on Github
+        </a>
+      </footer>
+    </div>
+  )
+}

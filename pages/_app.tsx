c6c12f2cf0cd529620ac8c258ab32c0e79fb2e6d
Callum Morris <me@callum.io>
2021-02-15 21:45:55 +0000

    Improve tooling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
+import type { AppProps } from 'next/app';
+import * as React from 'react';
 import '../styles/globals.css';
 
-function ValheimToolsApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function ValheimToolsApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
 export default ValheimToolsApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import * as React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -25,8 +26,8 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Open Source on Github
-    </a>
+        </a>
       </footer>
     </div >
-  )
+  );
 }

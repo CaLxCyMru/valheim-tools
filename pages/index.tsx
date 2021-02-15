2dffad5c492460d170a478e630ad22bf9cf57665
Callum Morris <me@callum.io>
2021-02-15 22:25:18 +0000

    Change coming soon text

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Coming Soon
+          Coming Soon!
         </p>
       </main>
       <footer className={styles.footer}>

3b49f2922aca7e0dd84aebf302b573fae1813b35
Callum Morris <me@callum.io>
2021-02-15 22:49:06 +0000

    Begin work on top navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,40 @@
 import Head from 'next/head';
 import * as React from 'react';
-import { Icon } from 'semantic-ui-react';
+import { Icon, Menu } from 'semantic-ui-react';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
+  const [activeItem, setActiveItem] = React.useState('home');
+
+  const handleItemClick = (event, { name }) => {
+    console.log(name);
+    setActiveItem(name);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Valheim Tools</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Menu>
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={handleItemClick}
+        >
+          Home
+        </Menu.Item>
+
+        <Menu.Item
+          name='seeds'
+          active={activeItem === 'seeds'}
+          onClick={handleItemClick}
+        >
+          Seeds
+        </Menu.Item>
+      </Menu>
 
 
       <main className={styles.main}>

d138370501c0218942c9d11b79faab43110cf1b3
Callum Morris <me@callum.io>
2021-02-18 13:47:08 +0000

    Add page to segment to split out from header/footer

--- components/hoc/WithLayout.tsx
+++ components/hoc/WithLayout.tsx
@@ -1,16 +1,19 @@
 import React, { createRef } from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Segment } from "semantic-ui-react";
 import { Footer, Header } from "../global";
+import styles from '../../styles/hoc/WithLayout.module.scss';
 
 export const withLayout = (Page) => {
   const appRef: React.Ref<any> = createRef();
 
   return ({ providers, ...props }) => (
-    <div ref={appRef}>
+    <div className={styles.layout} ref={appRef}>
       <Header providers={providers} />
-      <Container>
-        <Page {...props} />
-      </Container>
+      <Segment className={styles.page}>
+        <Container>
+          <Page {...props} />
+        </Container>
+      </Segment>
       <Footer />
     </div>
   );;

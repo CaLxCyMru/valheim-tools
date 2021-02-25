2c145c500de1c404eb9a3c9d59e0200ba03c20b5
Callum Morris <me@callum.io>
2021-02-25 12:41:52 +0000

    Create API Route for returning a single seeds info

---
+++ src/pages/api/seeds/[seed].ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Seed, SeedTag } from '../../../models';
+import { error } from '../../../utils';
+import { getRepo } from '../lib/seeds-db';
+
+const get = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { seed },
+  } = req;
+
+  if (!seed) {
+    error(res, 'Seed must be provided');
+    return;
+  }
+
+  const seedRepo = await getRepo<Seed>(Seed);
+  const data = await seedRepo.findOne({ where: { seed }, cache: 60000 });
+
+  if (!data) {
+    error(res, 'Seed does not exist');
+    return;
+  }
+
+  res.status(200).json(data);
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  switch (req.method) {
+    case 'GET':
+      await get(req, res);
+      break;
+    default:
+      res.status(405).end(); // Method Not Allowed
+      break;
+  }
+};
